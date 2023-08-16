70a25fc0b8fd614ab2b691c56ce38df633bd70de
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-08-16 15:44:08 -0300

    fix build

--- app/(navigation)/nosotros/page.tsx
+++ app/(navigation)/nosotros/page.tsx
@@ -1,11 +1,9 @@
-import BannerComponent from '@/components/BannerComponent/BannerComponent'
 import styles from './page.module.scss'
 import data from '@/models/es.json'
 
 export default function AboutUsPage() {
     return (
         <section id={data.aboutUs.sectionLink} className={styles['container-section-aboutus-page']}>
-            <BannerComponent imgData={data.aboutUs.image} />
         </section>
     )
 }
