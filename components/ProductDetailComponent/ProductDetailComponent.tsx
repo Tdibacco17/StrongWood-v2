a0bdacac5c5c2196ea9187edfbccde1585d5a8fb
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-09-05 11:43:02 -0300

    fix | product detail

--- components/ProductDetailComponent/ProductDetailComponent.tsx
+++ components/ProductDetailComponent/ProductDetailComponent.tsx
@@ -14,29 +14,27 @@ export default function ProductDetailComponent() {
     return (
         <div className={styles['container-section-product-detail']}>
             {productData ?
-                <>
-                    <div className={styles['container-card-prodcut-detail']}>
-                        <div className={styles['wrapper-header-product-detail']}>
-                            <ProductDetailGalleryContainer />
-                        </div>
-                        <ProductDetailInfoComponent />
+                <div className={styles['container-card-prodcut-detail']}>
+                    <div className={styles['wrapper-header-product-detail']}>
+                        <ProductDetailGalleryContainer />
                     </div>
-                    <div className={styles['description-product-detail']}>
-                        <p className={styles['title-description']}>{productData?.details.description.title}</p>
-                        <div className={styles['info-wrapper']}>
-                            {productData?.details.description.itemsDescription.map((text: string, index: number) => {
-                                return <p key={index} className={styles['info-description']}>{text}</p>
-                            })}
-                            {productData?.details.description.measures &&
-                                <p className={styles['info-description']}>{productData.details.description.measures}</p>}
-                        </div>
-                    </div>
-                </>
+                    <ProductDetailInfoComponent />
+                </div>
                 : <div className={styles['container-loader']}>
                     <div className={styles['position-loader']}>
                         <LoadingComponent />
                     </div>
                 </div>}
+            <div className={styles['description-product-detail']}>
+                <p className={styles['title-description']}>{productData?.details.description.title}</p>
+                <div className={styles['info-wrapper']}>
+                    {productData?.details.description.itemsDescription.map((text: string, index: number) => {
+                        return <p key={index} className={styles['info-description']}>{text}</p>
+                    })}
+                    {productData?.details.description.measures &&
+                        <p className={styles['info-description']}>{productData.details.description.measures}</p>}
+                </div>
+            </div>
         </div>
     )
 }
