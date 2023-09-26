9a55ee4276bdedb9485621761ce82a7ce35b1c07
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-09-26 16:53:53 -0300

    fix

--- app/(furniture)/dormitorio/page.tsx
+++ app/(furniture)/dormitorio/page.tsx
@@ -1,12 +1,13 @@
 import NavigationContainer from '@/containers/NavigationContainer/NavigationContainer'
 import styles from './page.module.scss'
 import CategorieStyleContainer from '@/containers/CategorieStyleContainer/CategorieStyleContainer'
+import data from '@/models/es.json'
 
 export default function BedroomPage() {
     return (
         <>
             <NavigationContainer isHome={false} showUrl={true} url={'/'} />
-            <section className={styles['container-section-bedroom-page']}>
+            <section id={data.designPage.sectionLink} className={styles['container-section-bedroom-page']}>
                 <CategorieStyleContainer designKey={'dormitorio'} />
             </section>
         </>
